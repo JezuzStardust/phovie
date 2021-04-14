0b1a549bef45aea3e7cb2972f699ad7f7dc42ea4
Jens Zamanian <jens.zamanian@gmail.com>
2021-04-14 15:01:58 +0200

    Add phobject and notes about writing effects.

---
+++ phobject.py
@@ -0,0 +1,12 @@
+# Define basic classes for handling things that should be added to Blender. 
+
+class Phobject():
+    """ Class to handle general Blender object.
+        Most of this will probably be a wrapper to real Blender's object class.
+    """
+        
+    # Type - E.g. CURVE, MESH, etc. 
+    # Reference - Name of Blender object within Blender. 
+    # Position - Coordinate of the object. 
+    # Rotation - Rotation in e.g. Euler. 
+    # Origin - Origin position relative to the object. E.g. UL, UR, etc. 
